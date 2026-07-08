12a526b29cf4e1baa7c584e6488b0f08faafb04e
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-08 23:32:21 +0530

    Longest Consecutive Length

---
+++ arrays&hasing/Main.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+
+public class Main {
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longest = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLength = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLength++;
+                }
+
+                longest = Math.max(longest, currentLength);
+            }
+        }
+
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        int answer = longestConsecutive(nums);
+
+        System.out.println(answer);
+    }
+}
