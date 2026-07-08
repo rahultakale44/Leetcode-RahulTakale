16c0ea1bb72071e127dcd7add97b408a31709b2b
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-09 00:00:10 +0530

    ProductExceptSelf

---
+++ arrays&hasing/ProductExceptSelf.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int left = 1;
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = left;
+            left = left * nums[i];
+        }
+
+        int right = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * right;
+            right = right * nums[i];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] answer = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(answer));
+    }
+}
