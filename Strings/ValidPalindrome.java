3c2b0cf57f4563803487b5a08223ac403643ac98
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-13 23:59:37 +0530

    Strings

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,43 @@
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+           
+            while (left < right &&
+                    !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            
+            while (left < right &&
+                    !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+
+            if (leftChar != rightChar) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+
+        boolean result = isPalindrome(s);
+
+        System.out.println("Input: " + s);
+        System.out.println("Is Valid Palindrome: " + result);
+    }
+}
