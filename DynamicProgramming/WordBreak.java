ebe6309009cfd346d87239e253e8c52ed26e9bb8
Rahul Takale <174298401+rahultakale44@users.noreply.github.com>
2026-07-27 22:51:54 +0530

    Word Break Problem

---
+++ DynamicProgramming/WordBreak.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class WordBreak {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+        HashSet<String> wordSet = new HashSet<>(wordDict);
+
+        boolean[] dp = new boolean[s.length() + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "leetcode";
+        List<String> dict1 = Arrays.asList("leet", "code");
+
+        String s2 = "applepenapple";
+        List<String> dict2 = Arrays.asList("apple", "pen");
+
+        String s3 = "catsandog";
+        List<String> dict3 = Arrays.asList("cats", "dog", "sand", "and", "cat");
+
+        System.out.println(wordBreak(s1, dict1)); // true
+        System.out.println(wordBreak(s2, dict2)); // true
+        System.out.println(wordBreak(s3, dict3)); // false
+    }
+}
